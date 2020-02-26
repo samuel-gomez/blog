b086c8756d0d4c5ec1053b24ab0ccaf29f2aad5c
Samuel Gomez <samuel.gomez@axa.fr>
2020-02-26 08:48:32 +0100

    feat(analytics): add tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-pixabay",
-      options: {
-        key: "13413439-f2e77375a5e4bfc5e4076a13c",
-        q: "yellow flowers",
-      },
-    },
-    {
       resolve: "gatsby-source-contract",
       options: {
         apiKey: "uL19TxbOTqdHcHTPd1AgQbR-FjqEDqWK",
@@ -40,6 +33,12 @@ module.exports = {
       options: {
         client_id: "uL19TxbOTqdHcHTPd1AgQbR-FjqEDqWK",
         client_secret: "9762db2dfc39e0b0e8d9988b89ebe8ad0221488e",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-131767530-1",
       },
     },
   ],
